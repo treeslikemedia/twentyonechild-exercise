d3e08ecebf1d6f394b394a2b511ffa5b2e10f348
treeslikemedia <treeslikeme@gmail.com>
2022-06-13 18:33:05 -0700

    initial commit

---
+++ header.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
+	<script type="text/javascript">
+	function letusknow() {
+	  alert('event triggered.')
+	}
+		
+	function customknow() {
+		alert('custom event');
+	}
+
+	// Add event listener to table
+	window.addEventListener("love", letusknow, false);
+	window.addEventListener("like", customknow, false);
+
+	</script>
+</head>
+
+<body <?php body_class('wicked'); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
+
+	<?php get_template_part( 'template-parts/header/site-header' ); ?>
+
+	<div id="content" class="site-content">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
